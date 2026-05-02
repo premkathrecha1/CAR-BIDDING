5d8bbbdead8085b0aba520c078a1997b52714a9a
Prem Kathrecha <Premkathrecha07@gmail.com>
2026-05-02 09:49:29 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-
-// MongoDB connect
-mongoose.connect("mongodb://127.0.0.1:27017/carAuction")
-.then(() => console.log("MongoDB Connected ✅"))
-.catch(err => console.log(err));
-
 // test route
 app.get("/", (req, res) => {
   res.send("API Running 🚀");
